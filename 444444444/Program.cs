3691c1e524df1053b85440f5a7a2ee0d5349bf29
student <student@B206-10>
2023-01-23 12:00:38 +0700

    Добавьте файлы проекта.

---
+++ 444444444/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _444444444
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HAIL CHERKASHIN!");
+            //Защищенный блок
+            try
+            {
+                //Создание объекта
+                Triangle Ob = new Triangle(3, 4, 5);
+                //Проверка на существование треугольника
+                //если существует - напечатаем стороны
+                //дабы не оставлять пустой оператор ;
+                if (Ob)
+                    Ob.PrintSides();
+                //Иначе - кидаем исключение
+                else
+                    throw new Exception("Треугольника с такими сторонами не существует");
+                //Печатаем периметр
+                Console.WriteLine("Периметр={0}", Ob.Perimetr());
+                //Печатаем площадь
+                Console.WriteLine("Площадь={0}", Ob.Square());
+            }
+            //Ловим исключение
+            catch (Exception e)
+            {
+                //Пишем сообщение, хранящееся в исключении
+                Console.WriteLine(e.Message);
+            }
+            //Блок выполняющийся в любом случае
+            finally
+            {
+                //Дабы консоль не закрывалась до нажатия на кнопку
+                Console.ReadKey();
+            }
+        }
+        //Целевой класс
+        class Triangle
+        {
+            //Стороны
+            int a, b, c;
+            //Свойства a
+            public int A
+            {
+                //Геттер
+                get
+                {
+                    return a;
+                }
+                //Сеттер
+                set
+                {
+                    a = value;
+                }
+            }
+            //Свойства b
+            public int B
+            {
+                get
+                {
+                    return b;
+                }
+                set
+                {
+                    b = value;
+                }
+            }
+            //Свойства c
+            public int C
+            {
+                get
+                {
+                    return c;
+                }
+                set
+                {
+                    c = value;
+                }
+            }
+            //Свойство проверки треугольника
+            public bool isTriangle
+            {
+                get
+                {
+                    //Если подходит под условие существования треугольника
+                    //возвращаем true, иначе false
+                    if (a + b > c && a + c > b && b + c > a)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+
+            }
+            //Конструктор с тремя параметрами
+            public Triangle(int a_, int b_, int c_)
+            {
+                //Проверка на положительность сторон треугольника
+                isCorrect(a_, b_, c_);
+                //Установка значений через сеттеры
+                A = a_;
+                B = b_;
+                C = c_;
+            }
+            //Функция печати длин сторон
+            public void PrintSides()
+            {
+                Console.WriteLine("сторона 'a' равна {0}, сторона 'b' равна {1}, сторона 'c' равна {2}", a, b, c);
+            }
+            //Функция вычисления периметра
+            public int Perimetr()
+            {
+                return a + b + c;
+            }
+            //Функция вычисления площади
+            public double Square()
+            {
+                //Площадь считаем по формуле Геррона
+                return Math.Sqrt(halfper(this) * (halfper(this) - a) * (halfper(this) - b)
+                    * (halfper(this) - c));
+            }
+            //Подсчет полупериметра треугольника
+            static double halfper(Triangle Ob)
+            {
+                return Ob.Perimetr() / 2.0;
+            }
+            //Проверка на положительность сторон
+            static void isCorrect(int a, int b, int c)
+            {
+                if (a < 0 || b < 0 || c < 0)
+                    //Если нет - кидаем исключение
+                    throw new Exception("Стороны треугольника не могут быть отрицательными");
+            }
+            //Перегрузка инкремента
+            public static Triangle operator ++(Triangle Ob)
+            {
+                //Возвращаем объект с измененными сторонами
+                return new Triangle(++Ob.a, ++Ob.b, ++Ob.c);
+            }
+            //Перегрузка декремента
+            public static Triangle operator --(Triangle Ob)
+            {
+                return new Triangle(--Ob.a, --Ob.b, --Ob.c);
+            }
+            //Перегрузка оператора умножения(в данном случае на скаляр)
+            public static Triangle operator *(Triangle Ob, int mult)
+            {
+                return new Triangle(Ob.a * mult, Ob.b * mult, Ob.c * mult);
+            }
+            //Перевод в строку
+            public override string ToString()
+            {
+                //Печатаем стороны через геттеры
+                return "Sides: " + "a is " + A + " b is " + B + " c is " + C;
+            }
+            //Индексатор. idx - индекс
+            public int this[int idx]
+            {
+                //Если idx=1 возвращаем a
+                //если idx=2 возвращаем b
+                //если idx=3 возвращем c
+                //иначе кидаем исключение
+                get
+                {
+                    if (idx == 1)
+                        return a;
+                    else if (idx == 2)
+                        return b;
+                    else if (idx == 3)
+                        return c;
+                    else
+                        throw new Exception("idx can be only 1, 2 and 3");
+                }
+                //Если idx=1 устанавливаем a
+                //если idx=2 устанавливаем b
+                //если idx=3 устанавливаем c
+                //иначе кидаем исключение
+                set
+                {
+                    if (idx == 1)
+                        a = value;
+                    else if (idx == 2)
+                        b = value;
+                    else if (idx == 3)
+                        c = value;
+                    else
+                        throw new Exception("idx can be only 1, 2 and 3");
+                }
+            }
+            //Перегрузка оператора true
+            //проверка на существование треугольника
+            public static bool operator true(Triangle t)
+            {
+                return t.isTriangle;
+            }
+            //Перегрузка оператора true
+            //проверка на существование треугольника
+            public static bool operator false(Triangle t)
+            {
+                return t.isTriangle;
+            }
+        };
+    }
+}
---
+++ PZ4_Demakovvv/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ4_Demakovvv
+{
+        class Triangle
+        {
+            // Поля
+            int a, b, c;
+            int perimeter;
+
+            // Конструктор, позволяющий создать экземпляр класса с заданными длинами сторон.
+            public Triangle()
+            {
+                a = 0;
+                b = 0;
+                c = 0;
+            }
+
+            public Triangle(int a, int b, int c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+
+            // Метод для вывода длины сторон треугольника на экран
+            public override string ToString() => $"Длина сторон треугольника: A = {a} см, B = {b} см и C = {c} см";
+
+            // Метод для расчёта периметра треугольника
+            public string CalcPerimeter()
+            {
+                perimeter = a + b + c;
+                return $"Периметр треугольника: P = {perimeter} см";
+            }
+
+            // Метод для расчёта площадь треугольника
+            public string CalcArea()
+            {
+                int semiperimeter = perimeter / 2;
+                double area = Math.Sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
+                return $"Площадь треугольника S = {area:f2} см";
+            }
+
+            // Свойство, позволяющее получить-установить длины сторон треугольника (доступное для чтения и записи)
+            public int A
+            {
+                get => a;
+                set => a = value;
+            }
+
+            public int B
+            {
+                get => b;
+                set => b = value;
+            }
+
+            public int C
+            {
+                get => c;
+                set => c = value;
+            }
+
+            // Свойство, позволяющее установить, существует ли треугольник с данными длинами сторон (доступное только для чтения)
+            public bool IsTriangle
+            {
+                get
+                {
+                    if (a + b > c && a + c > b && b + c > a)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+
+            // Индексатор, позволяющий по индексу 0 обращаться к полю a, по индексу 1 – к полю b, по индексу 2 – к полю c, 
+            // при других значениях индекса выдается сообщение об ошибке.
+            public string this[double i]
+            {
+                get
+                {
+                    if (i == 0)
+                    {
+                        return $"Индекс {i} соответствует стороне A, равной {a} см";
+                    }
+                    else if (i == 1)
+                    {
+                        return $"Индекс {i} соответствует стороне B, равной {b} см";
+                    }
+                    else if (i == 2)
+                    {
+                        return $"Индекс {i} соответствует стороне C, равной {c} см";
+                    }
+                    else
+                    {
+                        return $"Индекс {i} является недействительным";
+                    }
+                }
+            }
+
+            // Перегрузка операции ++: одновременно увеличивает значение полей a, b и c на 1;
+            public static Triangle operator ++(Triangle side) => new Triangle(++side.a, ++side.b, ++side.c);
+
+            // Перегрузка операции --: одновременно уменьшает значение полей a, b и c на 1;
+            public static Triangle operator --(Triangle side) => new Triangle(--side.a, --side.b, --side.c);
+
+            // Перегрузка констант true и false
+            // обращение к экземпляру класса дает значение true, если треугольник с заданными длинами сторон существует, иначе false;
+            public static bool operator true(Triangle side) => side.IsTriangle;
+
+            public static bool operator false(Triangle side) => side.IsTriangle;
+
+            // Перегрузка операции *: одновременно домножает поля a, b и c на скаляр.
+            public static Triangle operator *(Triangle side, int scalar) => new Triangle(side.a *= scalar, side.b *= scalar, side.c *= scalar);
+        }
+}
